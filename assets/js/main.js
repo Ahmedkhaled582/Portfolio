52966ab1630cf65d95323b7287b277771e48f569
Ahmedkhaled582 <ahmedd.khaled50@gmail.com>
2023-06-02 04:10:12 +0300

    portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+
+!(function($) {
+  "use strict";
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+
+})(jQuery);
+
+const age = document.getElementById('age');
+
+age.innerText = new Date().getFullYear() - 2002;
